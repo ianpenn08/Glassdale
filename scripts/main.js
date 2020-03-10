5b29905aec155988170fd103b6a6d9535ef8d15d
ianpenn08 <inkmpenn@gmail.com>
2020-03-10 15:24:02 -0500

    added conviction provider

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,12 @@
 // import {getOfficers} from "./officers/OfficerProvider.js"
+// import {OfficerList} from "./officers/"
 import criminalList from "./criminals/CriminalList.js"
+import ConvictionSelect from "./convictions/ConvictionSelect.js"
+import { getConvictions } from "./convictions/convictionProvider.js"
 
 criminalList()
+
+getConvictions().then(()=>{
+    ConvictionSelect()
+})
 // getOfficers()
