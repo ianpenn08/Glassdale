cc9c2bff61d2114cc8550f1fbd86bda05a8275bb
ianpenn08 <inkmpenn@gmail.com>
2020-03-18 13:44:31 -0500

    made api in note form and show all notes fully functional

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,17 @@
 import { getCriminals } from "./criminals/CriminalProvider.js"
 import { CriminalList } from "./criminals/CriminalList.js"
 import { getConvictions } from "./convictions/convictionProvider.js"
-import ConvictionSelect from "./convictions/ConvictionSelect.js"
-import NoteForm from "./notes/NoteForm.js"
+import  ConvictionSelect  from "./convictions/ConvictionSelect.js"
 import { DisplayNotesButton } from "./notes/DisplayNotesButton.js"
-import "./notes/NoteList.js"
 import { DisplayNoteFormButton } from "./notes/DisplayNoteFormButton.js"
+import  NoteForm  from "./notes/NoteForm.js"
+import { NotesList } from "./notes/NoteList.js"
 
 getCriminals().then(CriminalList)
-
-// first get all convictions, THEN create the conviction dropdown
 getConvictions().then(ConvictionSelect)
 
 DisplayNotesButton()
 DisplayNoteFormButton()
 NoteForm()
+NotesList()
+
